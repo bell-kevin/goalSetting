f45295b4dedea0ab530a8d913a0be97db14734a1
Kevin Bell <bellKevin@pm.me>
2023-02-28 16:04:50 -0700

    Add files via upload

---
+++ components/GoalInput.js
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Modal, Image } from 'react-native';
+
+function GoalInput(props) {
+    const [enteredGoalText, setEnteredGoalText] = useState('');
+
+    function goalInputHandler(enteredText) {
+        setEnteredGoalText(enteredText);
+    }
+
+    function addGoalHandler() {
+        props.onAddGoal(enteredGoalText);
+        setEnteredGoalText('');
+    }
+
+    return (
+        <Modal visible={props.modalIsVisible} animationType="slide">
+            <View style={styles.inputContainer}>
+                <Image source={require('../assets/images/goal.png')} style={styles.image} />
+                <TextInput
+                    placeholder="Your course goal"
+                    style={styles.textInput}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoalText}
+                />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="Cancel" onPress={props.onCancel} color='pink' />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Add goal" onPress={addGoalHandler} color='pink' />
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+};
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: 'purple',
+    },
+    image: {
+        width: 100,
+        height: 100,
+        margin: 20,
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: 'pink',
+        borderRadius: 6,
+        backgroundColor: 'pink',
+        color: 'black',
+        width: '100%',
+        padding: 16,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginTop: 16,
+    },
+    button: {
+        width: '30%',
+        marginHorizontal: 8,
+    },
+});
